f200acfeec3897cc1ea08beffd8e15f07ac00964
hamdi <hamdi@tanitweb.com>
2022-01-17 14:59:05 +0100

    create empty register and login page

---
+++ client/src/pages/register.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Register = props => {
+    return (
+        <div>
+            Register
+        </div>
+    )
+}
+
+Register.propTypes = {
+
+}
+
+export default Register
+
