1973cb060f5b05e7f037641f9029ab3ee16239f7
hamdi <hamdi@tanitweb.com>
2022-01-17 13:55:32 +0100

    fitler theme

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {BrowserRouter as  Router } from "react-router-dom"
 
 function App() {
   return (
+    <Router>
+     <input  type="checkbox" id="theme"/>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <div className="main">
+       hello hamdi</div>  
+  </div>
+  </Router>
   );
 }
 
