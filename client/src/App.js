439c5642880614cd9ba443a7ee0294807a6e109d
hamdi <hamdi@tanitweb.com>
2022-01-18 15:22:01 +0100

    add default route login

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 
 import {BrowserRouter as  Router,Route } from "react-router-dom"
 import PageRoute from "./PageRoute";
+import Login from "./pages/login";
 function App() {
   return (
     <Router>
      <input  type="checkbox" id="theme"/>
     <div className="App">
      <div className="main">
+       <Route exact path="/" component={Login}/>
        <Route exact path="/:page" component={PageRoute}/>
        <Route exact path="/:page/:id" component={PageRoute}/>
        
