14c0a5ed0bd62aa1db79dbb0abb9bfe251724524
hamdi <hamdi@tanitweb.com>
2022-01-21 17:02:20 +0100

    style header en cour

--- client/src/components/hedaer/Header.jsx
+++ client/src/components/hedaer/Header.jsx
@@ -6,15 +6,15 @@ import Search from "./Search";
 
 function Header(props) {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-between align-middle">
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
+    <div className="header bg-light">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-between align-middle">
+        <Link className="navbar-brand" to="/" className="logo">
           <h1 className="navbar-brand text-uppercase p-0 m-0">Social-media</h1>
         </Link>
         <Search />
         <Menu />
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 }
 
